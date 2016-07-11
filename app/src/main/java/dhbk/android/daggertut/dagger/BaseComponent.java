e366e32b1632d12855771b61b614f0016e808354
Phong <p7157568@gmail.com>
2016-07-11 22:59:59 +0700

    feat - create basecomponent

--- app/src/main/java/dhbk/android/daggertut/dagger/BaseComponent.java
+++ app/src/main/java/dhbk/android/daggertut/dagger/BaseComponent.java
@@ -1,8 +1,16 @@
 package dhbk.android.daggertut.dagger;
 
+import javax.inject.Singleton;
+
+import dagger.Component;
+import dhbk.android.daggertut.MVPApp;
+
 /**
  * Created by huynhducthanhphong on 7/11/16.
  */
+@Singleton
+@Component(modules={AppModule.class, OkModule.class})
 public interface BaseComponent {
-
+    MVPApp getMVPApp();
+    OKClient getOKClient();
 }
